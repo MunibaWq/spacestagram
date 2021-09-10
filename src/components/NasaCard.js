a86e793aa050501282626cfe7358263962c7e8fa
Muniba Waqar <muniba.wq@gmail.com>
2021-09-10 13:36:12 -0600

    rmv extra code

--- src/components/NasaCard.js
+++ src/components/NasaCard.js
@@ -14,8 +14,6 @@ import {
 import FavoriteIcon from "@material-ui/icons/Favorite";
 import ShareIcon from "@material-ui/icons/Share";
 
-import { red } from "@material-ui/core/colors";
-
 const useStyles = makeStyles((theme) => ({
   root: {
     maxWidth: 345,
@@ -23,19 +21,6 @@ const useStyles = makeStyles((theme) => ({
   media: {
     height: 0,
     paddingTop: "56.25%", // 16:9
-  },
-  expand: {
-    transform: "rotate(0deg)",
-    marginLeft: "auto",
-    transition: theme.transitions.create("transform", {
-      duration: theme.transitions.duration.shortest,
-    }),
-  },
-  expandOpen: {
-    transform: "rotate(180deg)",
-  },
-  avatar: {
-    backgroundColor: red[500],
   },
 }));
 
