80e82a0f90ca55b55a5e8205b1afc37a7f835f96
Muniba Waqar <muniba.wq@gmail.com>
2021-09-13 21:37:33 -0600

    mapping through api results

--- src/components/CardContainer.js
+++ src/components/CardContainer.js
@@ -8,17 +8,23 @@ const CardContainer = () => {
   useEffect(() => {
     const getNasaInfo = async () => {
       const res = await getNasaMars();
-      setNasaInfo(res.data);
+      setNasaInfo(res.data.photos);
     };
     getNasaInfo();
   }, []);
 
-  console.log(nasaInfo);
-
   return (
-    <>
-      <NasaCard />
-    </>
+    <div>
+      {nasaInfo
+        ? nasaInfo.map((array, index) => {
+            return (
+              <>
+                <NasaCard key={array.earth_date} array={array} />
+              </>
+            );
+          })
+        : null}
+    </div>
   );
 };
 
