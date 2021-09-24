3822cbd1a79d0dd6b96109427b6cd95b14a65a5f
Muniba Waqar <muniba.wq@gmail.com>
2021-09-24 15:58:51 -0600

    get photos from NASA api

--- backend/routes/Nasa.js
+++ backend/routes/Nasa.js
@@ -1,8 +1,24 @@
 const express = require("express");
 const router = express.Router();
+const axios = require("axios");
 
-router.get("/", (req, res) => {
-  res.send("Picture: #1");
+require("dotenv").config();
+
+router.get("/", async (req, res, next) => {
+  try {
+    let response = await axios.get(
+      `https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&a`,
+      {
+        params: {
+          api_key: process.env.REACT_APP_NASA_API_KEY,
+        },
+      }
+    );
+
+    res.send(response.data.photos);
+  } catch (err) {
+    next(err);
+  }
 });
 
 module.exports = router;
