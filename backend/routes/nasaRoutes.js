46609ef6d7d61af894ad79b3c94cad555c736696
Muniba Waqar <muniba.wq@gmail.com>
2021-09-29 11:25:11 -0600

    fixed get request

--- backend/routes/nasaRoutes.js
+++ backend/routes/nasaRoutes.js
@@ -4,17 +4,17 @@ const axios = require("axios");
 
 require("dotenv").config();
 
-router.get("/", async (req, res, next) => {
+router.get("/nasa", async (req, res, next) => {
   try {
     let response = await axios.get(
-      `https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&a`,
+      `https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&`,
       {
         params: {
           api_key: process.env.REACT_APP_NASA_API_KEY,
         },
       }
     );
-
+    // console.log(response.data);
     res.send(response.data);
   } catch (err) {
     next(err);
