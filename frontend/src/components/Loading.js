3a3b59ef9d132aa8d3eb40ab8b5cc6062d63bce2
Muniba <muniba.wq@gmail.com>
2021-10-04 11:58:58 -0600

    add loading component

---
+++ frontend/src/components/Loading.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import LoadingIcon from '../assets/loading.svg';
+
+const Loading = () => {
+  return <div></div>;
+};
+
+export { Loading };
--- frontend/src/components/index.js
+++ frontend/src/components/index.js
@@ -2,3 +2,4 @@ export * from './NasaCard.js';
 export * from './CardContainer.js';
 export * from './Navbar.js';
 export * from './Hero.js';
+export * from './Loading.js';
