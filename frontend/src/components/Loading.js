21e6185d4f276861150a49a4a38aa79c52b16e2c
Muniba <muniba.wq@gmail.com>
2021-10-04 13:36:21 -0600

    add spinner animation

--- frontend/src/components/Loading.js
+++ frontend/src/components/Loading.js
@@ -1,9 +1,26 @@
+import { makeStyles } from '@material-ui/core';
 import React from 'react';
 import LoadingIcon from '../assets/loading.svg';
 
+const useStyles = makeStyles(theme => ({
+  spinner: {
+    position: 'relative',
+    animation: '$spin 5s infinite'
+  },
+  '@keyframes spin': {
+    '0%': {
+      transform: 'rotate(180deg)'
+    },
+    '100%': {
+      transform: 'rotate(0deg)'
+    }
+  }
+}));
+
 const Loading = () => {
+  const classes = useStyles();
   return (
-    <div>
+    <div className={classes.spinner}>
       <img src={LoadingIcon} alt='loading' />
     </div>
   );
