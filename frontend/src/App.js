e2a59b7d99b5fce6ccaca1f9a837d369d695d507
Muniba <muniba.wq@gmail.com>
2021-10-07 13:45:17 -0600

    add main wrapper styling and theme

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,20 @@
-import './App.css';
+import { makeStyles } from '@material-ui/core';
 import { CardContainer, Hero, Navbar } from './components';
 
+const useStyles = makeStyles(theme => ({
+  app: {
+    margin: 0,
+    textAlign: 'center',
+    background: '#222121',
+    minHeight: '100vh',
+    color: theme.palette.primary.main
+  }
+}));
+
 function App() {
+  const classes = useStyles();
   return (
-    <div className='App'>
+    <div className={classes.app}>
       <Navbar />
       <Hero />
       <CardContainer />
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './index.css';
 import App from './App';
 import { ThemeProvider } from '@material-ui/styles';
 import { theme } from './theme/theme';
